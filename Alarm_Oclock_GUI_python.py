6ea9103cc22f888c9c2dd954c540a2513727aa74
Yapnounkie <edmundopierre@yahoo.com>
2021-08-08 23:57:03 -0700

    create the alarm function

--- Alarm_Oclock_GUI_python.py
+++ Alarm_Oclock_GUI_python.py
@@ -4,3 +4,17 @@ import datetime
 import time
 import winsound
 
+#Create the function Alarm
+
+def alarm(set_alarm):
+
+    while True:
+        time.sleep(1)
+        current_time = datetime.datetime.now()
+        time_now = current_time.strftime("%H:%M:%S")
+        date_now = current_time.strftime("%d/%m/%Y")
+        print(time_now)
+        if time_now == set_alarm:
+            print("It is time to get  up sleep head")
+        winsound.PlaySound("sound.wav",winsound.SND_ASYNC)
+        break    
